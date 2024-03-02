36f0c3b52d3c238a9e626db4ff54d5299a7545ec
Maximiliano <143568715+Gatomaxxpro@users.noreply.github.com>
2024-03-02 13:17:52 -0700

    Add files via upload

---
+++ Examen_MOSTRAR_INFO_ALUM/src/examen_mostrar_info_alum/CLASES.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_mostrar_info_alum;
+
+/**
+ *
+ * @author MAX
+ */
+ 
+
+public class CLASES {
+    
+
+   private  String nombre;
+   private String apellido;
+   private String nmc;
+   
+   
+  
+
+
+public String getnoombre (){
+    return nombre;
+}
+
+public void setnombre(String valor){
+    nombre = valor;
+    
+    
+}
+//apellifo
+ public String getapellido (){
+    return apellido;
+}
+
+public void setapellido(String valor){
+   valor =apellido;
+    
+    
+} 
+
+public String getnmc (){
+    return  nmc;
+}
+
+public void setnmc(String valor){
+   nmc = valor;
+
+}
+public void imprimirdatos(){
+   
+}
+
+}
+
