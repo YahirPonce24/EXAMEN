0e918dd90df1ba6a9687a304d35892cd216db890
Atzlares <158379505+Atzlares@users.noreply.github.com>
2024-03-03 23:50:25 -0600

    Update empleado.java
    Añadido de calcular vacaciones

--- clase_empleado/src/clase_empleado/empleado.java
+++ clase_empleado/src/clase_empleado/empleado.java
@@ -16,6 +16,24 @@ public class empleado {
     private int anio_ingreso;
     private double salario;
 
+    public Empleado() {
+        //Constructor sin parametro
+        this.nombre = "Jorge";
+        this.apellido = "Lares";
+        this.direccion = "Calle imaginario 3434";
+        this.anio_ingreso = 2023;
+        this.salario = 500.0;
+    }
+    
+    public Empleado(String nombre, String apellido, String direccion, int anio_ingreso, double salario) {
+        //Con parametro
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.anio_ingreso = anio_ingreso;
+        this.salario = salario;
+    }
+
     
     //get y set
     
@@ -49,14 +67,28 @@ public class empleado {
     public void setSalario (double valor){
     salario = valor;
     }
-    
-    public void imp_datos (){
-        System.out.println(" nombre del empleado es "+nombre + " "+apellido);
+    //Calcular vacaciones
+    public int calcularVacaciones() {
+        int antiguedad = 2024 - anio_ingreso;
+        //6 días de vacaciones para un año de antigüedad
+        int diasVacaciones = 6; 
 
-        System.out.println("La direccion del usuario es "+direccion);
-        System.out.println("El año de ingreso es "+anio_ingreso);
-        System.out.println("El salario es "+salario);
+        //Dos días de vacaciones por cada año adicional
+        diasVacaciones += (antiguedad - 1) * 2;
+
+        //Dos días de vacaciones por cada 5 años laborados
+        diasVacaciones += (antiguedad / 5) * 2;
+
+        return diasVacaciones;
+    }
     
+    public void imprimirDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Año de Ingreso: " + anio_ingreso);
+        System.out.println("Salario: " + salario);
+        System.out.println("Días de Vacaciones: " + calcularVacaciones());
     }
     
 
